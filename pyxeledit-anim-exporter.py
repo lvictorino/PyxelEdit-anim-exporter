9cf43ec8f7b3822d5393d286ee3b527bdd7634bb
Laurent Victorino <lvictorino@monkeymoon.net>
2014-04-01 17:08:05 +0200

    Now flavored with Exceptions and Comments.

--- pyxeledit-anim-exporter.py
+++ pyxeledit-anim-exporter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import zipfile
 import json
 import sys
@@ -7,27 +8,54 @@ import os
 
 if len(sys.argv) < 2:
     print "usage: pyxeledit-anim-exporter.py project.pyxel"
-    sys.exit
+    sys.exit(0)
 
-file_path = sys.argv[1]
-zfile = zipfile.ZipFile(file_path)
-for name in zfile.namelist():
-    (dirname, filename) = os.path.split(name)
-    if filename == "docData.json":
-        zfile.extract(name, dirname)
-        json_data=open(name)
-        pyxel_data=json.load(json_data)
-        data = {}
-        data['animations'] = pyxel_data['animations']
-        data['tileset'] = pyxel_data['tileset']
-        exported_file = pyxel_data['name'] + "-anim.json"
-        print "Creation of " + exported_file + "..."
-        with open(exported_file, 'w') as outfile:
-            json.dump(data, outfile)
-        json_data.close()
-        os.remove(name)
-        print "Animation properties of " + pyxel_data['name'] + " project have been successfully exported."
-zfile.close()
+try:
+    extracted = False
+    project_name = ""
+    file_path = sys.argv[1]
+    # Unzip pyxel file
+    zfile = zipfile.ZipFile(file_path)
+    # List all files contained to find 'docData.json'
+    for name in zfile.namelist():
+        (dirname, filename) = os.path.split(name)
+        if filename == "docData.json":
 
+            try:
+                # Extract docData.json from zip
+                zfile.extract(name, dirname)
+            except IOError:
+                print "Error you're not allowed to create files in the current directory."
+                sys.exit(1)
 
+            # Load it as a JSON file
+            json_data=open(name)
+            pyxel_data=json.load(json_data)
+            data = {}
+            try:
+                # Get interesting information for animations
+                data['animations'] = pyxel_data['animations']
+                data['tileset'] = pyxel_data['tileset']
+                project_name = pyxel_data['name']
+                # Create and fill a file named after the project name with animation properties
+                exported_file = project_name + "-anim.json"
+                with open(exported_file, 'w') as outfile:
+                    json.dump(data, outfile)
+                json_data.close()
+                # Remove extracted file
+                os.remove(name)
+                extracted = True
+            except KeyError:
+                print "Error: the content of .pyxel file is not valid."
+            break
+    
+    zfile.close()
+
+    if extracted is not True:
+        print "Error: the content of .pyxel file is not valid."
+    else:
+        print "Animation properties of " + project_name + " project have been successfully exported."
+
+except IOError:
+    print "Error: given file is not valid. Please give a .pyxel file as parameter."
 
