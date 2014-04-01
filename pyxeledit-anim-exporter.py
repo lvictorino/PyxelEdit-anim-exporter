8c376225f4aebbddc0ab84ca769af5931b71563c
Laurent Victorino <lvictorino@monkeymoon.net>
2014-04-01 16:08:28 +0200

    PyxelEdit project JSON load and animation properties export.

--- pyxeledit-anim-exporter.py
+++ pyxeledit-anim-exporter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import zipfile
+import json
 import sys
 import os.path
 import os
@@ -13,9 +14,20 @@ zfile = zipfile.ZipFile(file_path)
 for name in zfile.namelist():
     (dirname, filename) = os.path.split(name)
     if filename == "docData.json":
-        print "Decompressing " + filename
         zfile.extract(name, dirname)
-
+        json_data=open(name)
+        pyxel_data=json.load(json_data)
+        data = {}
+        data['animations'] = pyxel_data['animations']
+        data['tileset'] = pyxel_data['tileset']
+        exported_file = pyxel_data['name'] + "-anim.json"
+        print "Creation of " + exported_file + "..."
+        with open(exported_file, 'w') as outfile:
+            json.dump(data, outfile)
+        json_data.close()
+        os.remove(name)
+        print "Animation properties of " + pyxel_data['name'] + " project have been successfully exported."
+zfile.close()
 
 
 
