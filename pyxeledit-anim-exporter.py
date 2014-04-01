e3ab785facd49ac23721a6d9a6426af37af49dcc
Laurent Victorino <lvictorino@monkeymoon.net>
2014-04-01 14:56:07 +0200

    PyxelEdit docData.json extraction + argument handling

--- pyxeledit-anim-exporter.py
+++ pyxeledit-anim-exporter.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+import zipfile
+import sys
+import os.path
+import os
 
-print "hello world."
+if len(sys.argv) < 2:
+    print "usage: pyxeledit-anim-exporter.py project.pyxel"
+    sys.exit
+
+file_path = sys.argv[1]
+zfile = zipfile.ZipFile(file_path)
+for name in zfile.namelist():
+    (dirname, filename) = os.path.split(name)
+    if filename == "docData.json":
+        print "Decompressing " + filename
+        zfile.extract(name, dirname)
+
+
+
 
